bd3ed652624cc2d4feb5660dc05ea2317c834fc2
Andrei Floricel <andrei.floricel@gmail.com>
2023-12-08 17:23:12 +0200

    Update to latest versions

--- src/app/columnDefs.ts
+++ src/app/columnDefs.ts
@@ -1,45 +1,116 @@
 import { ColDef } from '@ag-grid-community/core';
+import { WebFramework } from './rowData';
 
-// create AG Grid Column Definitions
-export const columnDefs:ColDef[] = [
+export const defaultColDef: ColDef = {
+  resizable: true,
+  sortable: true,
+  editable: true,
+  filter: true,
+  floatingFilter: true,
+};
+
+export const columnDefs: ColDef<WebFramework>[] = [
   {
-    colId: 'id',
-    hide: true,
-    suppressColumnsToolPanel:true,
-    suppressFiltersToolPanel:true
-  },
-  {
-    headerName: 'Auto Make',
-    field: 'make',
-    editable: true,
-    filter: true,
-    floatingFilter: true,
-    sortable: true,
-    type: 'abColDefString',
-  },
-  {
-    headerName: 'Model',
-    field: 'model',
-    editable: true,
-    filter: true,
-    floatingFilter: true,
-    sortable: true,
-    type: 'abColDefString',
-  },
-  {
-    headerName: 'Price',
-    field: 'price',
-    editable: true,
-    filter: true,
-    floatingFilter: true,
-    sortable: true,
+    field: 'id',
     type: 'abColDefNumber',
+    hide: true,
+    editable: false,
   },
   {
-    headerName: 'Date manufactured',
-    field: 'date',
+    field: 'name',
+    type: 'abColDefString',
+    sortable: true,
+  },
+
+  {
+    field: 'language',
+    type: 'abColDefString',
+    enablePivot: true,
+    enableRowGroup: true,
+  },
+  {
+    field: 'github_stars',
+    headerName: 'GitHub Stars',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  {
+    field: 'license',
+    type: 'abColDefString',
+    editable: false,
+    enablePivot: true,
+    enableRowGroup: true,
+  },
+  {
+    field: 'week_issue_change',
+    headerName: 'Issue Change',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  {
+    field: 'created_at',
+    headerName: 'Created',
     type: 'abColDefDate',
-    filter: true,
-    floatingFilter: true,
+  },
+  {
+    field: 'has_wiki',
+    headerName: 'Has Wiki',
+    type: 'abColDefBoolean',
+    enablePivot: true,
+    enableRowGroup: true,
+  },
+  {
+    field: 'updated_at',
+    headerName: 'Updated',
+    type: 'abColDefDate',
+  },
+  {
+    field: 'pushed_at',
+    headerName: 'Pushed',
+    type: 'abColDefDate',
+  },
+  {
+    field: 'github_watchers',
+    headerName: 'GitHub Watchers',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  {
+    field: 'description',
+    type: 'abColDefString',
+    sortable: false,
+  },
+  {
+    field: 'open_issues_count',
+    headerName: 'Open Issues',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  {
+    field: 'closed_issues_count',
+    headerName: 'Closed Issues',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+
+  {
+    field: 'open_pr_count',
+    headerName: 'Open PRs',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  {
+    field: 'closed_pr_count',
+    headerName: 'Closed PRs',
+    type: 'abColDefNumber',
+    enableValue: true,
+  },
+  { field: 'has_projects', headerName: 'Has Projects', type: 'abColDefBoolean' },
+  { field: 'has_pages', headerName: 'Has Pages', type: 'abColDefBoolean' },
+  {
+    field: 'topics',
+    type: 'abColDefString',
+    editable: false,
+    sortable: false,
   },
 ];
